38b5cefc91b739b67223f10a696418d379c36473
Nicolas Andalaft <nicolas.andalaft@hotmail.com>
2021-03-29 13:28:13 -0300

    Adicionados arquivos do projeto Dart

---
+++ bin/fake_artist_bot.dart
@@ -0,0 +1,5 @@
+import 'package:fake_artist_bot/fake_artist_bot.dart' as fake_artist_bot;
+
+void main(List<String> arguments) {
+  print('Hello world: ${fake_artist_bot.calculate()}!');
+}
---
+++ test/fake_artist_bot_test.dart
@@ -0,0 +1,8 @@
+import 'package:fake_artist_bot/fake_artist_bot.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
